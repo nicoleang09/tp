507598586d2b39e4cd8d74afdcd864b3facbeee9
nicoleang09 <nicoleang09@gmail.com>
2021-02-23 15:28:35 +0800

    Add remark command

---
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -0,0 +1,36 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.RemarkCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.Remark;
+
+public class RemarkCommandParser implements Parser {
+    /**
+     * Parses {@code userInput} into a command and returns it.
+     *
+     * @param args
+     * @throws ParseException if {@code userInput} does not conform the expected format
+     */
+    @Override
+    public Command parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_REMARK);
+
+        Index index;
+
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (ParseException pe) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, RemarkCommand.MESSAGE_USAGE), pe);
+        }
+
+        Remark remark = new Remark(argMultimap.getValue(PREFIX_REMARK).orElse(""));
+
+        return new RemarkCommand(index, remark);
+    }
+}
